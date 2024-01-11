3ae40baf206ec43d0b50df2fe519d3189902eb79
Ali Farahmand <alifarahmand1993@gmail.com>
2024-01-11 10:08:49 -0500

    update footer and menu component
    Conditional Rendering With &&

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const Footer = () => {
-    const hour = new Date();
-    const currentDateTime = hour.toLocaleString();
+    // const hour = new Date();
+    // const currentDateTime = hour.toLocaleString();
+    const hour = new Date().getHours();
+    const openHour = 9;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
 
     return (
-        <div>{currentDateTime}</div>
+        <footer className='footer'>
+            {isOpen && (
+                <div className='order'>
+                    <p>We're open until {closeHour}:00 come visit us or order online</p>
+                    <button className='btn'>Order</button>
+                </div>
+            )}
+        </footer>
     )
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -49,18 +49,23 @@ const pizzaData = [
 
 
 const Menu = () => {
+    const pizzas = pizzaData;
+    // const pizzas = [];
+    const numPizzas = pizzas.length;
     return (
         <main className='menu '>
             <h2>Our Menu</h2>
-            <ul className='pizzas'>
+            {numPizzas > 0 && (<ul className='pizzas'>
                 {/* {pizzaData.map((pizza) => (<Pizza
                     name={pizza.name}
                     photoName={pizza.photoName}
                     price={pizza.price}
                     ingredients={pizza.ingredients}
                 />))} */}
-                {pizzaData.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
+                {pizzas.map((pizza) => (<Pizza pizzaObj={pizza} key={pizza.name} />))}
             </ul>
+            )}
+
         </main>
     )
 }
